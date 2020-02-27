71527949bfb875180b6b33ced9020ebe30e02c11
Quimey <zerox_94_mm@hotmail.com>
2020-02-27 00:18:12 -0300

    [ADD] mails route.

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const app = express();
 const router = express.Router();
 const publicPath = path.join(__dirname, "..", "public");
 
+const mails_mocks = require('./mails.mock.json');
+
 router.post('/auth/signin', (req, res) => {
   const { email, password } = req.body;
 
@@ -21,6 +23,15 @@ router.post('/auth/signin', (req, res) => {
   }
 
   return res.status(200).send({ isLogged: true });
+});
+
+router.get(/mails|drafts|sents/ig, (req, res) => {
+  const { page = 0, pagination = 20 } = req.params;
+  const startIndex = page * pagination;
+
+  const mails = mails_mocks.slice(startIndex, startIndex + pagination - 1);
+
+  return res.status(200).send(mails);
 });
 
 app.use(express.json());
