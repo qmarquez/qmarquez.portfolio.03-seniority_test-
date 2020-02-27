dfacee0a8fb816e9bc51fbec65baa1901672dd25
Quimey <zerox_94_mm@hotmail.com>
2020-02-27 02:29:54 -0300

    [MOD] mail service.

--- src/app/services/mail.service.ts
+++ src/app/services/mail.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, httpRequest } from '../lib/httpRequest';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
 import { Mails } from '../models/mail.interface';
 
 @Injectable({
@@ -17,8 +16,11 @@ export class MailService {
     this.httpRequest = httpRequest(httpClient, '');
   }
 
-  getMails(type) {
-    return this.httpRequest<Mails>('GET', type);
+  getMails(type: string, page: number) {
+    const params = {
+      page: page.toString()
+    }
+    return this.httpRequest<Mails>('GET', type, { params });
   }
 
 }
