e66d4f7209c9a7f32152fd3f07b2797dac820126
Quimey Marquez <qmarquez@est.frba.utn.edu.ar>
2020-02-28 13:10:46 -0300

    [FIX] bad apiURL on production.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import { baseEnvironment } from './environment.base';
 
 baseEnvironment.production = true;
-baseEnvironment.apiURL = 'https://dummy_mailer.herokuapp.com/api';
+baseEnvironment.apiURL = 'https://dummy-mailer.herokuapp.com/api';
 
 export { baseEnvironment as environment };
